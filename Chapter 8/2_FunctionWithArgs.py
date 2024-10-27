aa3c1d9433103ed7791b2e90a4cbddcbfe1bbae7
ChampatiSatyabrata <141308691+collision-hp@users.noreply.github.com>
2024-10-27 22:04:18 +0530

    Delete Chapter 8 directory

--- Chapter 8/2_FunctionWithArgs.py
+++
@@ -1,9 +0,0 @@
-##Function with argument
-def goodDay(name, ending):
-    print("Good day" , name)
-    print(ending)
-    return 'Done'
-a=goodDay("Stokes" , "Thank You")
-print(a)
-print(goodDay("King" ,"Thank you"))
-print(goodDay("Gill" , "Thanks"))
