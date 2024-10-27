6169e8e94bbd6aa7cca905954157e06264bda0af
ChampatiSatyabrata <141308691+collision-hp@users.noreply.github.com>
2024-10-27 22:03:08 +0530

    Delete Chapter 3 directory

--- Chapter 3/3_Function.py
+++
@@ -1,9 +0,0 @@
-str="manifestation"
-print(len(str))
-print(str.endswith("ion")) #like comparison returns boolean values
-print(str.startswith("Manif")) #case sensitive
-print(str.capitalize()) #Capitalizes the first letter only
-print(str.upper())
-
-updated_string = str.replace('a' , 'z')
-print(updated_string)
